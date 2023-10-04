dbb8290b20f21eb5b48d384eabf7c5589b6f1493
BinyaminR <binyamin.rothenberg@gmail.com>
2023-10-04 18:32:27 +0300

    fixed

---
+++ infrastructure/terraform_commands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+# Change to the correct directory
+cd infrastructure
+
+# Initialize Terraform
+echo "Initializing Terraform..."
+terraform init
+
+echo "Checking Terraform format..."
+terraform fmt -check
+
+# Plan Terraform changes
+echo "Planning Terraform configurations..."
+terraform plan -input=false
+
+# Apply Terraform changes
+echo "Applying Terraform configurations..."
+terraform apply -auto-approve
+
+echo "All tasks completed successfully!"
